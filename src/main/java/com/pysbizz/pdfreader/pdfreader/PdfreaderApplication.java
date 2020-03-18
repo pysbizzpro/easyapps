848cd9dae1af59c4401e2f81c4c509abaf61e055
pdf reader <mdjamilsm@gmail.com>
2020-03-18 13:51:55 +0530

    vanilla

---
+++ src/main/java/com/pysbizz/pdfreader/pdfreader/PdfreaderApplication.java
@@ -0,0 +1,13 @@
+package com.pysbizz.pdfreader.pdfreader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfreaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdfreaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pysbizz/pdfreader/pdfreader/PdfreaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pysbizz.pdfreader.pdfreader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdfreaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
